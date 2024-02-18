014b394a885c5a474b750910cc7ad27b7442e976
Rocky Dey <coderrocky25@gmail.com>
2024-02-18 14:29:52 +0600

    booking price section complete

--- js/function.js
+++ js/function.js
@@ -2,6 +2,7 @@ const bookedSeats = [];
 function seatId(event) {
   if (bookedSeats.length < 4) {
     bookedSeats.push(event.target);
+    console.log(bookedSeats);
     selectSeat(event.target.id);
     const showSeat = document.getElementById("show-seat");
     const span = document.createElement("span");
@@ -14,11 +15,32 @@ function seatId(event) {
       <p>550</p>
     </div>`;
     showSeat.appendChild(span);
+
+    // Button enable
     if (bookedSeats.length === 4) {
       document.getElementById("apply-btn").removeAttribute("disabled");
     }
+
+    // Update Value
     document.getElementById("total-price").innerText = bookedSeats.length * 550;
+    document.getElementById("available-seat").innerText =
+      40 - bookedSeats.length;
+    document.getElementById("seat-booked").innerText = bookedSeats.length;
+
+    // Coupon validation
+    document.getElementById("apply-btn").addEventListener("click", getCode);
   } else {
     alert("maximum 4 seats only");
+  }
+}
+
+function getCode() {
+  const couponCode = document.getElementById("coupon-code").value;
+  if (couponCode === "NEW15") {
+    discount(couponCode);
+  } else if (couponCode === "Couple 20") {
+    discount(couponCode);
+  } else {
+    alert("Please provide valid coupon");
   }
 }
--- js/utility.js
+++ js/utility.js
@@ -1,3 +1,35 @@
 function selectSeat(elementId) {
   document.getElementById(elementId).style.backgroundColor = "#1DD100";
+  document.getElementById(elementId).style.color = "white";
+}
+
+function discount(elementId) {
+  const inputField = document.getElementById("input-field");
+  console.log(inputField);
+  const totalPrice = document.getElementById("total-price").innerText;
+  const span = document.createElement("span");
+  if (elementId === "NEW15") {
+    span.innerHTML = `
+    <div class="text-base text-color1 font-medium flex justify-between items-center">
+        <p>Discount</p>
+        <p>BDT <span id="discount-price">${
+          parseInt(totalPrice) * 0.15
+        }</span></p>
+    </div>`;
+    inputField.innerHTML = "";
+    inputField.appendChild(span);
+  } else {
+    span.innerHTML = `
+    <div class="text-base text-color1 font-medium flex justify-between items-center">
+        <p>Discount</p>
+        <p>BDT <span id="discount-price">${
+          parseInt(totalPrice) * 0.2
+        }</span></p>
+    </div>`;
+    inputField.innerHTML = "";
+    inputField.appendChild(span);
+  }
+  const discountPrice = document.getElementById("discount-price").innerText;
+  document.getElementById("grand-total").innerText =
+    parseInt(totalPrice) - parseInt(discountPrice);
 }
