1505446b7d6f7ff04ef0f8fe55fd4c1dbec6bd9f
Rocky Dey <coderrocky25@gmail.com>
2024-02-18 16:05:14 +0600

    next button implement and add modal

--- js/function.js
+++ js/function.js
@@ -2,12 +2,11 @@ const bookedSeats = [];
 function seatId(event) {
   if (bookedSeats.length < 4 && !bookedSeats.includes(event.target.id)) {
     bookedSeats.push(event.target.id);
-    console.log(bookedSeats);
     selectSeat(event.target.id);
     const showSeat = document.getElementById("show-seat");
     const span = document.createElement("span");
     span.innerHTML = `
-    <div id=${event.target.id} class="text-base text-color6 font-medium flex justify-between items-center">
+    <div class="text-base text-color6 font-medium flex justify-between items-center">
       <p>
       ${event.target.id}
       </p>
@@ -29,12 +28,18 @@ function seatId(event) {
 
     // Coupon validation
     document.getElementById("apply-btn").addEventListener("click", getCode);
+
+    // next button enable
+    document
+      .getElementById("input-number")
+      .addEventListener("blur", formValidation);
   } else if (bookedSeats.includes(event.target.id)) {
     // bookedSeats.pop(event.target.id);
     // unSelectSeat(event.target.id);
   } else {
     alert("maximum 4 seats only");
   }
+  formValidation();
 }
 
 function getCode() {
@@ -45,5 +50,13 @@ function getCode() {
     discount(couponCode);
   } else {
     alert("Please provide valid coupon");
+  }
+}
+
+function formValidation() {
+  const phoneNumber = document.getElementById("input-number").value;
+  console.log(phoneNumber);
+  if (bookedSeats.length > 0 && phoneNumber) {
+    document.getElementById("next-btn").removeAttribute("disabled");
   }
 }
